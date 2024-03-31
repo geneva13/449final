68ef672783dbba05554e465da80f2a8fbcb4bac6
Geneva Jacobs <144170448+geneva13@users.noreply.github.com>
2024-03-31 22:35:48 +0000

    tutorial changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+
+const movielist = [
+  {id: 1, title: 'Scott Pilgrim VS The World', isAvailable: true},
+  {id: 2, title: 'SpiderMan Far from Home', isAvailable: true},
+  {id: 3, title: 'Elf (2003)', isAvailable: false},
+];
+const Book = {
+title: 'Back to the Future',
+director: 'Robert Zemeckis',
+};
+function MovieRack() {
+  const listMovies = movielist.map(movielist =>
+    <li
+    key={movielist.id}
+    style={{
+      color: movielist.isAvailable ? 'green' : 'red'
+    }}
+  >
+  {movielist.title}
+  </li>
+  );
+  return (
+    <ul>{listMovies}</ul>
+  )
+}
+
+function Bookshelf() {
+  return(
+    <>
+    <p>Title: {Book.title}</p>
+    <p>Director: {Book.director}</p>
+    
+    </>
+  )
+}
 
 function MagicButton() {
   return (
+    <>
+  <h3>This is a magic button</h3>
   <button>Magic</button>
-
+  </>
   )
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I Edited <code>src/App.js</code> and saved to reload!.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I, Geneva Jacobs, Learned React with Jeff and MSU!
-        </a>
+      <header className="App-header"> 
+      <h2>Our Current Movie Recommendation:</h2>
+
+        <Bookshelf />
+      <h2>Trending Titles: (Green if IMBD rating over 8)</h2>
+        <MovieRack />
       </header>
-      <MagicButton />
     </div>
   );
 }
