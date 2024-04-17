d3ea5b276bb271a0dd7a502ec602ce7d198c0c79
Geneva Jacobs <144170448+geneva13@users.noreply.github.com>
2024-04-17 17:02:39 +0000

    built structure

---
+++ src/AdviceComponent.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+
+const AdviceComponent = () => {
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    const fetchAdvice = async () => {
+      try {
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        const { advice } = response.data.slip;
+        setAdvice(advice);
+      } catch (error) {
+        console.error('Error fetching advice:', error);
+      }
+    };
+
+    fetchAdvice();
+  }, []);
+
+  return (
+    <Card className="bg-light custom-card">
+      <Card.Body className="text-center my-4">
+        <Card.Title>Advice Slip</Card.Title>
+        <Card.Text>{advice}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default AdviceComponent;
--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-//import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import AdviceComponent from './AdviceComponent'; 
+import yourImage from './affirmations.png'; // Import your image
 
 const movielist = [
   {id: 1, title: 'Scott Pilgrim VS The World', isAvailable: true},
@@ -8,19 +14,20 @@ const movielist = [
   {id: 3, title: 'Elf (2003)', isAvailable: false},
 ];
 const Book = {
-title: 'Back to the Future',
-director: 'Robert Zemeckis',
+  title: 'Back to the Future',
+  director: 'Robert Zemeckis',
 };
+
 function MovieRack() {
   const listMovies = movielist.map(movielist =>
     <li
-    key={movielist.id}
-    style={{
-      color: movielist.isAvailable ? 'green' : 'red'
-    }}
-  >
-  {movielist.title}
-  </li>
+      key={movielist.id}
+      style={{
+        color: movielist.isAvailable ? 'green' : 'red'
+      }}
+    >
+      {movielist.title}
+    </li>
   );
   return (
     <ul>{listMovies}</ul>
@@ -30,28 +37,94 @@ function MovieRack() {
 function Bookshelf() {
   return(
     <>
-    <p>Title: {Book.title}</p>
-    <p>Director: {Book.director}</p>
-    
+      <p>Title: {Book.title}</p>
+      <p>Director: {Book.director}</p>
     </>
   )
 }
 
-
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"> 
-      <h2>Our Current Movie Recommendation:</h2>
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [books, setBooks] = useState([]);
 
-        <Bookshelf />
-      <h2>Trending Titles: (Green if IMBD rating over 8)</h2>
-        <MovieRack />
+  useEffect(() => {
+    getRandomQuote();
+    getBooks();
+  }, []);
+
+  async function getRandomQuote() {
+    let resp = await axios.get(`https://dummyjson.com/quotes/random`);
+    setQuote(resp.data.quote);
+    setAuthor(resp.data.author);
+  }
+
+  async function getBooks() {
+    try {
+      const response = await fetchSupabaseData();
+      setBooks(response);
+    } catch (error) {
+      console.error('Error fetching books:', error);
+    }
+  }
+
+  async function fetchSupabaseData() {
+    const supabaseUrl = 'https://bulkffxplgttqshuifys.supabase.co';
+    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1bGtmZnhwbGd0dHFzaHVpZnlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NzYyMjMsImV4cCI6MjAyNjM1MjIyM30.8zvPt_sRI68lEu5l-_1IllS10bO4xbfWKaXMRcZhqEQ';
+    const supabase = createClient(supabaseUrl, supabaseKey);
+    let { data: books, error } = await supabase.from('books').select('*');
+    return books;
+  }
+
+  return (
+    <div className='App'>
+      <header className="App-header"> 
+        <h2>Our Current Movie Recommendation:</h2>
       </header>
+      <div className='d-flex justify-content-center align-items-center vh-100'>
+        <div>
+        <h2>Advice Slip</h2>
+        <p>: reload every 2 seconds to draw a new piece of advice</p>
+          <AdviceComponent /> 
+          <h2>Quote Generator</h2>
+
+          <Card className='bg-light custom-card'>
+            <Card.Body className='text-center my-4'>
+              <Card.Text>
+                "{quote}"
+              </Card.Text>
+              <Card.Text className='text-end'>
+                --- {author}
+              </Card.Text>
+              <Button className='bg-navy mt-3' onClick={getRandomQuote}>
+                Generate Random Quote
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+      <div>
+        <h2>Supabase Books:</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Publisher</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={index}>
+                <td>{book.title}</td>
+                <td>{book.publisher}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <img src={yourImage} alt="Your Image" style={{ width: '600px', height: 'auto' }} />
+      </div>
     </div>
   );
 }
 
 export default App;
- 
